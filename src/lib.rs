36d10e69b0d392c60a403ceabd2d4fc056d45df8
范辉 <fanhui@songguo7.com>
2020-02-26 03:58:56 +0000

    add Debug binding

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,14 @@
 ///
 ///! # All errors will be converted to MyError.
 ///
-use std::{error::Error, fmt};
+use std::{
+    error::Error,
+    fmt::{Debug, Display},
+};
 
 pub type Result<T> = std::result::Result<T, Box<dyn MyError>>;
 
-pub trait MyError: fmt::Display + Send {
+pub trait MyError: Display + Debug + Send {
     fn cause(&mut self) -> Option<Box<dyn MyError>> {
         None
     }
@@ -22,36 +25,37 @@ pub trait MyError: fmt::Display + Send {
 }
 
 pub trait MyResult<T> {
-    fn c(self, msg: impl fmt::Display) -> Result<T>;
+    fn c(self, msg: impl Display) -> Result<T>;
 }
 
 impl<T> MyResult<T> for Result<T> {
-    fn c(self, msg: impl fmt::Display) -> Result<T> {
+    fn c(self, msg: impl Display) -> Result<T> {
         self.map_err(|e| MiniError::new(msg, Some(e)).into())
     }
 }
 
 impl<T> MyResult<T> for Option<T> {
-    fn c(self, msg: impl fmt::Display) -> Result<T> {
+    fn c(self, msg: impl Display) -> Result<T> {
         self.ok_or_else(|| MiniError::new(msg, None).into())
     }
 }
 
 impl<T, E: Error> MyResult<T> for std::result::Result<T, E> {
-    fn c(self, msg: impl fmt::Display) -> Result<T> {
+    fn c(self, msg: impl Display) -> Result<T> {
         self.map_err(|e| {
             MiniError::new(msg, Some(Box::new(MiniError::new(e, None)))).into()
         })
     }
 }
 
+#[derive(Debug)]
 struct MiniError {
     msg: String,
     cause: Option<Box<dyn MyError>>,
 }
 
 impl MiniError {
-    fn new(msg: impl fmt::Display, cause: Option<Box<dyn MyError>>) -> Self {
+    fn new(msg: impl Display, cause: Option<Box<dyn MyError>>) -> Self {
         MiniError {
             msg: format!("{}", msg),
             cause,
@@ -59,8 +63,8 @@ impl MiniError {
     }
 }
 
-impl fmt::Display for MiniError {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+impl Display for MiniError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         write!(f, "{}", self.msg)
     }
 }
