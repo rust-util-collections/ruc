851f7abfb6e907d1360dc9a98303400de877327a
fh <hui.fan@mail.ru>
2023-07-05 22:41:15 +0800

    Revert "BASE64_URL_SAFE_NO_PAD"
    This reverts commit c68487620c27e7c875f72724d3172e2d994b6db3.

--- src/crypto/codec/base64/mod.rs
+++ src/crypto/codec/base64/mod.rs
@@ -1,9 +1,9 @@
 use crate::*;
-use base64::prelude::{Engine, BASE64_URL_SAFE_NO_PAD};
+use base64::prelude::{Engine, BASE64_STANDARD};
 
 #[inline(always)]
 pub fn encode<T: AsRef<[u8]>>(orig: T) -> String {
-    BASE64_URL_SAFE_NO_PAD.encode(orig)
+    BASE64_STANDARD.encode(orig)
 }
 
 #[inline(always)]
@@ -13,5 +13,5 @@ pub fn decode(encoded: &str) -> Result<Vec<u8>> {
 
 #[inline(always)]
 pub fn decode_generic<T: AsRef<[u8]>>(encoded: T) -> Result<Vec<u8>> {
-    BASE64_URL_SAFE_NO_PAD.decode(encoded).c(d!())
+    BASE64_STANDARD.decode(encoded).c(d!())
 }
