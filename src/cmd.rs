437cd322c83f7a8b2447a628338e743c6745a832
fh <dafanhui@163.com>
2025-07-16 17:13:32 +0800

    tunning timeout logic

--- src/cmd.rs
+++ src/cmd.rs
@@ -56,11 +56,23 @@ fn exec_spawn_poll(mut child: Child) -> Result<PollRet> {
 /// and return its outputs after `exit`,
 /// or panic with a `timeout` error
 pub fn exec_timeout(cmd: &str, timeout_milliseconds: u64) -> Result<String> {
+    if 0 == timeout_milliseconds {
+        return exec(cmd).c(d!());
+    }
+
     let mut child = exec_spawn(cmd).c(d!())?;
     let mut try_times = timeout_milliseconds / 100;
 
+    let rm = timeout_milliseconds % 100;
+    let pre_wait_time = if 0 < rm {
+        rm
+    } else {
+        try_times -= 1;
+        100
+    };
+    sleep_ms!(pre_wait_time);
+
     loop {
-        sleep_ms!(100);
         match exec_spawn_poll(child).c(d!())? {
             PollRet::Ready(o) => {
                 if o.status.success() {
@@ -75,6 +87,7 @@ pub fn exec_timeout(cmd: &str, timeout_milliseconds: u64) -> Result<String> {
                 child = c;
                 if 0 < try_times {
                     try_times -= 1;
+                    sleep_ms!(100);
                 } else {
                     info_omit!(child.kill());
                     return Err(eg!("Process time out!"));
